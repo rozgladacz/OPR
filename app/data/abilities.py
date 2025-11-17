6069f70a0a32bd04277f19b7ab5946c9214d9f74
rozgladacz <rozgladacz@gmail.com>
2025-11-17 10:37:35 +0100

    Add Mobilizacja active ability

--- app/data/abilities.py
+++ app/data/abilities.py
@@ -296,6 +296,12 @@ ABILITY_DEFINITIONS: List[AbilityDefinition] = [
         description="Oddział w zasięgu 2” odrzuca k3 znaczniki ran.",
     ),
     AbilityDefinition(
+        slug="mobilizacja",
+        name="Mobilizacja",
+        type="active",
+        description="Oddział w zasięgu 12” przestaje być przyszpilony.",
+    ),
+    AbilityDefinition(
         slug="rozkaz",
         name="Rozkaz",
         type="active",
