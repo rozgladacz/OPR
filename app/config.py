270ec1ab5057fa144a25a30f07bc7d0e780478dd
rozgladacz <rozgladacz@gmail.com>
2026-01-04 08:52:12 +0100

    typo

--- app/config.py
+++ app/config.py
@@ -12,4 +12,4 @@ SECRET_KEY = os.getenv("SECRET_KEY", "jakis_dlugi_i_sekretny_klucz_po_polsku_dla
 DB_URL = os.getenv("DB_URL", "sqlite:///./data/opr.db")
 DEBUG = os.getenv("DEBUG", "false").lower() in {"1", "true", "yes"}
 UPDATE_REPO_URL = os.getenv("UPDATE_REPO_URL", "https://github.com/rozgladacz/OPR")
-UPDATE_BRANCH = os.getenv("UPDATE_BRANCH", "ain")
+UPDATE_BRANCH = os.getenv("UPDATE_BRANCH", "main")
