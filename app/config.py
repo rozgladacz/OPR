0539c4e0f5d9654b6da29f14587e83cf9417c51b
rozgladacz <rozgladacz@gmail.com>
2026-01-04 08:49:46 +0100

    Set default update repository configuration

--- app/config.py
+++ app/config.py
@@ -11,5 +11,5 @@ DATA_DIR.mkdir(exist_ok=True)
 SECRET_KEY = os.getenv("SECRET_KEY", "jakis_dlugi_i_sekretny_klucz_po_polsku_dla_zmylki")
 DB_URL = os.getenv("DB_URL", "sqlite:///./data/opr.db")
 DEBUG = os.getenv("DEBUG", "false").lower() in {"1", "true", "yes"}
-UPDATE_REPO_URL = os.getenv("https://github.com/rozgladacz/OPR")
-UPDATE_BRANCH = os.getenv("main")
+UPDATE_REPO_URL = os.getenv("UPDATE_REPO_URL", "https://github.com/rozgladacz/OPR")
+UPDATE_BRANCH = os.getenv("UPDATE_BRANCH", "ain")
