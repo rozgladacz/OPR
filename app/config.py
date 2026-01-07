c7ea2234adf29cbaebde2414d2f7b34c331398f0
rozgladacz <rozgladacz@gmail.com>
2026-01-07 10:38:54 +0100

    Add update path and build configuration env vars

--- app/config.py
+++ app/config.py
@@ -13,3 +13,7 @@ DB_URL = os.getenv("DB_URL", "sqlite:///./data/opr.db")
 DEBUG = os.getenv("DEBUG", "false").lower() in {"1", "true", "yes"}
 UPDATE_REPO_URL = os.getenv("UPDATE_REPO_URL", "https://github.com/rozgladacz/OPR")
 UPDATE_BRANCH = os.getenv("UPDATE_BRANCH", "main")
+UPDATE_REPO_PATH = os.getenv("UPDATE_REPO_PATH", ".")
+UPDATE_REF = os.getenv("UPDATE_REF", "")
+UPDATE_DOCKERFILE = os.getenv("UPDATE_DOCKERFILE", "Dockerfile")
+UPDATE_COMPOSE_FILE = os.getenv("UPDATE_COMPOSE_FILE", "docker-compose.yml")
